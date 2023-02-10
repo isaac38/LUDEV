53b3412a9329872aa2e300159a2eb4aaf3cdec39
Luis Isaac Muñoz <luisisaacmunozg@gmail.com>
2023-02-09 18:51:30 -0700

    cambios en arquitectura css y menu toggle responsivo

--- js/index.js
+++ js/index.js
@@ -1,26 +1,47 @@
-$(document).ready(function() {
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        items: 4,
-        loop: true,
-        margin: 200,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
-        dots: false,
-        nav: false,
-        lazyLoad: true,
-        center: true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
+
+//stea el hash de la url para, al recargar se regrese a la primera parte
+window.location.hash = '#portada'
+
+
+//nloke que controla el modificador active del link menu
+const links = document.querySelectorAll('.header__link')
+links.forEach((items, i) => {
+    items.addEventListener('click', function () {
+
+
+        links.forEach(elementos => elementos.classList.remove('header__link--active'))
+
+        items.classList.add('header__link--active')
+    })
 })
+
+const headerBtnMenu = document.getElementById('headerBtnMenu')
+const sidebarBtnMenu = document.getElementById('sidebarBtnMenu')
+const sidebar = document.querySelector('.sidebar')
+
+headerBtnMenu.addEventListener('click', function () { 
+    sidebar.classList.remove('sidebar--close')
+    sidebar.classList.add('sidebar--open')
+})
+
+sidebar.addEventListener('click', function () { 
+    sidebar.classList.remove('sidebar--open')
+    sidebar.classList.add('sidebar--close')
+})
+
+
+
+
+//***** */
+
+
+const linksSkills = document.querySelectorAll('.skills__li')
+linksSkills.forEach((items, i) => {
+    items.addEventListener('click', function () {
+        linksSkills.forEach(elementos => elementos.classList.remove('skills__li--active'))
+
+        items.classList.add('skills__li--active')
+    })
+})
+
+
